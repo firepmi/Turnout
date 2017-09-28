34856853e6e26c9f7adc6a063c27268f25301726
thelastsummer <goldieblonde08@gmail.com>
2017-09-28 14:49:27 -0700

    Initial Commit

---
+++ Turnout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Turnout
+//
+//  Created by Mobile World on 9/28/17.
+//  Copyright © 2017 Coca Denisa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
